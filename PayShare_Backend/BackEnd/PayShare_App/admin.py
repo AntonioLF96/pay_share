4afa2c2c0592f0028cf1b1d609b41517db475b51
unknown <antonio.lafemina96@gmail.com>
2023-05-31 20:49:41 +0200

    admin.py

--- PayShare_Backend/BackEnd/PayShare_App/admin.py
+++ PayShare_Backend/BackEnd/PayShare_App/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models  import Transaction, BancomatSmartContract, SmartContract, Bancomat, User, Group
+
 # Register your models here.
+
+admin.site.register(Transaction)
+admin.site.register(User)
+admin.site.register(Bancomat)
+admin.site.register(SmartContract)
+admin.site.register(BancomatSmartContract)
+admin.site.register(Group)
