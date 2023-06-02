c3d90e95130c76acf5b435995f0ea6c604ff5619
progettoappsplit@gmail.com <progettoappsplit@gmail.com>
2023-06-02 11:23:47 +0200

    registration

--- PayShare_Backend/BackEnd/BackEnd/urls.py
+++ PayShare_Backend/BackEnd/BackEnd/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from BackEnd.PayShare_App import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', views.register_user, name='register_user'),
 ]
--- PayShare_Backend/BackEnd/PayShare_App/views.py
+++ PayShare_Backend/BackEnd/PayShare_App/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
-# Create your views here. ok
+from BackEnd.PayShare_App.models import User
+
+
+@csrf_exempt
+def register_user(request):
+    if request.method == 'POST':
+        # Ottieni i dati inviati dalla richiesta POST
+        code = request.POST.get('code')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        fiscal_code = request.POST.get('fiscal_code')
+        birth_date = request.POST.get('birth_date')
+        birth_place = request.POST.get('birth_place')
+        residence = request.POST.get('residence')
+
+        # Crea un nuovo oggetto User
+        user = User(
+            code=code,
+            password=password,
+            email=email,
+            fiscalCode=fiscal_code,
+            birthDate=birth_date,
+            birthPlace=birth_place,
+            residence=residence
+        )
+
+        # Salva l'oggetto User nel database
+        user.save()
+
+        # Restituisci una risposta JSON di conferma
+        return JsonResponse({'message': 'User registered successfully.'})
+
+    # Restituisci un errore se viene effettuata una richiesta diversa da POST
+    return JsonResponse({'error': 'Invalid request method.'})
+
